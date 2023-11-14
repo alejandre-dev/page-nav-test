deca21129e300a85e514faa3e70b46e083b93501
alex <alex@alejandre.dev>
2023-11-14 12:04:08 -0600

    [ADD] refactor to use the section rendering api

--- assets/page-nav.js
+++ assets/page-nav.js
@@ -1,15 +1,9 @@
 function handlePageNav(linkURL, linkHandle) {
   if (history.state && history.state.handle) {
     const prevLinkHandle = history.state.handle;
-    const inactivePage = document.getElementById(`page-nav__page--${prevLinkHandle}`);
-    inactivePage.style.display = "none";
-
     const inactiveButton = document.getElementById(`page-nav__button--${prevLinkHandle}`);
     inactiveButton.classList.remove("page-nav__button--active");
   }
-
-  const activePage = document.getElementById(`page-nav__page--${linkHandle}`);
-  activePage.style.display = "block";
 
   const activeButton = document.getElementById(`page-nav__button--${linkHandle}`);
   activeButton.classList.add("page-nav__button--active");
@@ -19,4 +13,6 @@ function handlePageNav(linkURL, linkHandle) {
   const nextURL = linkURL;
 
   window.history.replaceState(nextState, unused, nextURL);
+
+  window.dispatchEvent(new CustomEvent("page-nav:change"));
 }
